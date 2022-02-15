15867256acc03da7084c3040636dded4c6b0d165
Cleyton Carvalho <cleyton.carvalho@outlook.com>
2022-02-14 21:17:09 -0300

    refactoring: cpf validation

--- src/cpf.ts
+++ src/cpf.ts
@@ -1,7 +1,7 @@
 import { InvalidCpfException } from "./cpf-exceptions";
 
 export class Cpf {
-  private readonly cpf: string;
+  private cpf: string;
 
   get value(): string {
     return this.cpf;
@@ -13,36 +13,44 @@ export class Cpf {
   }
 
   static create(cpf: string): Cpf {
-    return new Cpf(cpf.replace(/\D/g, ""));
+    return new Cpf(cpf);
   }
 
   private validateCpf(): boolean {
     if (!this.cpf) return false;
-    if (this.cpf.length != 11) return false;
-    if (this.areAllDigitsEqual()) return false;
+    this.cleanCpf();
+    if (this.isCpfLengthValid()) return false;
+    if (this.areAllCpfDigitsEqual()) return false;
     return this.calculateValidCpf() === this.cpf;
   }
 
-  private areAllDigitsEqual(): boolean {
-    return this.cpf.split("").every((digit) => digit === this.cpf[0]);
+  private cleanCpf() {
+    this.cpf = this.cpf.replace(/\D/g, "");
+  }
+
+  private isCpfLengthValid(): boolean {
+    return this.cpf.length != 11;
+  }
+
+  private areAllCpfDigitsEqual(): boolean {
+    const [firstDigit] = this.cpf[0];
+    return [...this.cpf].every((digit) => digit === firstDigit);
   }
 
   private calculateValidCpf(): string {
-    const firstNineDigits = this.cpf.substring(0, 9);
-    const verifyingDigitOne = this.calculateVerifyingDigit(firstNineDigits);
-    const firstTenDigits = firstNineDigits.concat(verifyingDigitOne);
-    const verifyingDigitTwo = this.calculateVerifyingDigit(firstTenDigits);
-    return firstTenDigits.concat(verifyingDigitTwo);
+    const firstNineDigits = this.cpf.slice(0, 9);
+    const verifyingDigitOne = this.calculateCpfVerifyingDigit(firstNineDigits);
+    const firstTenDigits = `${firstNineDigits}${verifyingDigitOne}`;
+    const verifyingDigitTwo = this.calculateCpfVerifyingDigit(firstTenDigits);
+    return `${firstTenDigits}${verifyingDigitTwo}`;
   }
 
-  private calculateVerifyingDigit(digits: string): string {
+  private calculateCpfVerifyingDigit(digits: string): string {
     let multiplier = digits.length + 1;
-    const sum = digits
-      .split("")
+    const sum = [...digits]
       .map((digit) => +digit * multiplier--)
       .reduce((partialSum, currentValue) => partialSum + currentValue);
     const rest = sum % 11;
-    if (rest < 2) return "0";
-    return (11 - rest).toString();
+    return rest < 2 ? "0" : `${11 - rest}`;
   }
 }
