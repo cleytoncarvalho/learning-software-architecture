4834e686e0b920165908c96ecabc15882ce2cc2f
Cleyton Carvalho <cleyton.carvalho@outlook.com>
2022-02-24 14:46:56 -0300

    test: place order without coupon

--- src/application/usecases/place-order/PlaceOrder.test.ts
+++ src/application/usecases/place-order/PlaceOrder.test.ts
@@ -18,6 +18,25 @@ test("place order", () => {
       { itemId: 1, quantity: 1 },
       { itemId: 3, quantity: 2 },
     ],
+  });
+  expect(output.total).toBe(80);
+});
+
+test("place order with coupon", () => {
+  const orderRepository = new OrderRepositoryMemory();
+  const itemRepository = new ItemRepositoryMemory();
+  const couponRepository = new CouponRepositoryMemory();
+  const placeOrder = new PlaceOrder(
+    orderRepository,
+    itemRepository,
+    couponRepository
+  );
+  const output = placeOrder.execute({
+    cpf: "516.178.806-20",
+    orderItems: [
+      { itemId: 1, quantity: 1 },
+      { itemId: 3, quantity: 2 },
+    ],
     coupon: "VALE10",
   });
   expect(output.total).toBe(73);
