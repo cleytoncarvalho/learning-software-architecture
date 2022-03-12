d3b6edfe698e97a29aae1093136b697ad1b7c0fe
Cleyton Carvalho <cleyton.carvalho@outlook.com>
2022-03-12 12:35:14 -0300

    feature: item repository database

---
+++ src/infra/repositories/ItemRepositoryDatabase.test.ts
@@ -0,0 +1,18 @@
+import { PostgreSQLConnectionAdapter } from "../database/adapters/PostgreSQLConnectionAdapter";
+import { ItemRepositoryDatabase } from "./ItemRepositoryDatabase";
+
+test("get item by id using PostgreSQL", async () => {
+  const postgreSQLConnectionAdapter = new PostgreSQLConnectionAdapter();
+  const itemRepositoryDatabase = new ItemRepositoryDatabase(
+    postgreSQLConnectionAdapter
+  );
+  const item = await itemRepositoryDatabase.getById(1);
+  postgreSQLConnectionAdapter.close();
+  expect(item?.itemId).toBe(1);
+  expect(item?.description).toBe("Guitarra");
+  expect(item?.price).toBe(1000);
+  expect(item?.width).toBe(100);
+  expect(item?.height).toBe(50);
+  expect(item?.depth).toBe(15);
+  expect(item?.weight).toBe(3);
+});
---
+++ src/infra/repositories/ItemRepositoryDatabase.ts
@@ -0,0 +1,24 @@
+import { Item } from "../../domain/entities/item/Item";
+import { ItemRepository } from "../../domain/repositories/ItemRepository";
+import { Connection } from "../database/Connection";
+
+export class ItemRepositoryDatabase implements ItemRepository {
+  constructor(readonly connection: Connection) {}
+
+  async getById(itemId: number): Promise<Item | undefined> {
+    const [result] = await this.connection.query(
+      "select * from ccca.item where id_item = $1",
+      [itemId]
+    );
+
+    return new Item({
+      itemId: result.id_item,
+      description: result.description,
+      price: parseFloat(result.price),
+      width: parseInt(result.width),
+      height: parseInt(result.height),
+      depth: parseInt(result.length),
+      weight: parseInt(result.weight),
+    });
+  }
+}
